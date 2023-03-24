18c127a4842f8b955de556c710a681ab172963d1
Ana Warner <aswarner@umich.edu>
2023-03-24 16:30:48 -0400

    fix broken main

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ import sensor_msgs
 def callback_imu(data):
     print(data)
 
+def callback_odom(data):
+    print(data)
+
+def callback_dvl(data):
+    print(data)
+
 ## subscribe to bagfile topics
-## put data in ISAM
 
 if __name__=='__main__':
     rospy.init_node('data_listener', anonymous=True)
 
-    rospy.Subscriber('/mavros/imu/data_raw', sensor_msgs.Imu, callback_imu)
+    rospy.Subscriber('/mavros/imu/data_raw', sensor_msgs.msg.Imu, callback_imu)
+    rospy.Subscriber('/mavros/imu/data_raw', sensor_msgs.msg.Imu, callback_odom)
+    rospy.Subscriber('/mavros/imu/data_raw', sensor_msgs.msg.Imu, callback_dvl)
 
     rospy.spin()
