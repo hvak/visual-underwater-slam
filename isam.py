9d940702efb51ec789f73b356433bc24801b4529
Ana Warner <aswarner@umich.edu>
2023-03-25 13:52:32 -0400

    update isam

--- isam.py
+++ isam.py
@@ -3,25 +3,28 @@ import numpy as np
 from matplotlib import pyplot as plt
 from gtsam.symbol_shorthand import B, V, X
 
+import gtsam
+from gtsam import (ISAM2, BetweenFactorConstantBias, Cal3_S2,
+                   ConstantTwistScenario, ImuFactor, NonlinearFactorGraph,
+                   PinholeCameraCal3_S2, Point3, Pose3,
+                   PriorFactorConstantBias, PriorFactorPose3,
+                   PriorFactorVector, Rot3, Values)
+
 BIAS_KEY = B(0)
 
+def vector3(x, y, z):
+    """Create 3d double numpy array."""
+    return np.array([x, y, z], dtype=float)
 
 
-''' 
-copied from slam hw needs editing lol
-TODO:
-    inputs, noise, 
-
-    Parameters: factors, edges
-    
-'''
 class AUV_ISAM:
-
+    ##Iniotialize parameters for the IMU
     def preintegration_parameters(self):
         # IMU preintegration parameters
         # Default Params for a Z-up navigation frame, such as ENU: gravity points along negative Z-axis
         PARAMS = gtsam.PreintegrationParams.MakeSharedU(g)
         I = np.eye(3)
+        # later TODO: Set covariance to be actual IMU covariance
         PARAMS.setAccelerometerCovariance(I * 0.1)
         PARAMS.setGyroscopeCovariance(I * 0.1)
         PARAMS.setIntegrationCovariance(I * 0.1)
@@ -32,84 +35,110 @@ class AUV_ISAM:
 
         return PARAMS, BIAS_COVARIANCE
 
+
+    ##Retrieve and update ROS data as quickly as it is received.  Store it in various data structures
     def update_imu(self, data):
-        measAcc = np.array([data.linear_acceleration.x, data.linear_acceleration.y, data.linear_acceleration.z])
-        measOmega = np.array([data.angular_velocity])
+        measAcc = np.array([data.linear_acceleration.x, data.linear_acceleration.y, data.linear_acceleration.z]) - np.dot([self.g_transform, self.g])
+        measOmega = np.array([data.angular_velocity.x, data.angular_velocity.y, data.angular_velocity.z])
+        self.imu = np.array([measAcc, measOmega])
         return
 
     def update_odom(self, data):
+        self.odom = {"x": data.pose.pose.position.x, 
+                     "y": data.pose.pose.position.y,
+                     "z": data.pose.pose.position.z,
+                     "i" : data.pose.pose.orientation.x,
+                     "j" : data.pose.pose.orientation.y,
+                     "k" : data.pose.pose.orientation.z, 
+                     "q" : data.pose.pose.orientation.w}
         return
 
     def update_dvl(self, data):
+        self.dvl = {"x" : data.twist.linear.x,
+                    "y" : data.twist.linear.y,
+                    "z" : data.twist.linear.z}
         return
 
     def update_orb(self, data):
+        #self.orb = np.array([data.landmarkSeen, data.landMarkId])
+
         return
 
+    ##Create factors for the graph based on ROS datab
+    def create_imu_factor(self):
+        delta_t = .01
+        self.accum.integrateMeasurement(self.imu[0], self.imu[1], delta_t)
+        imuFactor = ImuFactor(X(self.timestep - 1), V(self.timestep - 1), X(self.timestep), V(self.timestep), BIAS_KEY, self.accum)
+        return imuFactor
+    
+    def create_dvl_factor(self):
+        dvlFactor = None
+        return dvlFactor
+    
+    def create_depth_factor(self):
+        depthFactor = None
+        return depthFactor
 
+    def create_orb_factor(self):
+        orbFactor = None
+        return orbFactor
+    
+    # returns odometry as gtsam pose3 object
+    def get_odom_as_pose3(self):
+        t = gtsam.Point3( self.odom['x'], 
+                        self.odom['y'], 
+                        self.odom['y'])
+        rot = gtsam.Rot3(self.odom['q'], self.odom['i'], self.odom['j'], self.odom['k'])
+        odom = gtsam.Pose3(rot, t)
+        return odom
+    
     def __init__(self):
-        ## TODO PRIORS
+        ## TODO Define Prior Noise
         self.priorPose =  gtsam.pose3(np.eye(3), np.zeros(3))
         self.priorNoise = None
-        self.priorVel = None
+        self.priorVel = gtsam.pose3(np.eye(3), np.zeros(3))
         self.priorVelNoise = None
 
-        ## TODO Sensor inits
-        ## most recent sensor data
+        ## stores most recent data from rosbag
         self.imu = None
         self.odom = None
         self.dvl = None
         self.orb = None
-        self.g = vector3(0, 0, -g)
-
-        self.optimized_poses = None ## result from isam optimization  :) ## same as isam.estimate() ## dont need
+        self.g = vector3(0, 0, -9.81)
         self.isam = gtsam.gtsam.ISAM2()
+        
+        self.timestep = 0
+        PARAMS, BIAS_COV = self.preintegration_parameters()
+        self.accum = gtsam.PreintegratedImuMeasurements(PARAMS)
 
-    '''
-    TODO: this function should be scheduled to run periodically and 
-    convert all most recent data variables (self.dvl, self.imu, etc)
-    to factors and add them to factor 
-    '''
-    def create_factors_from_recent():
-        pass
+        self.initial_estimate = Values()
+        self.g_transform = 0
 
-
+     
     def update_isam(self, vertices, edges):
+        
+        graph = gtsam.gtsam.NonlinearFactorGraph()
+        initial_estimate = gtsam.Values()
+        if self.timestep == 0:
+            graph.push_back(gtsam.gtsam.PriorFactorPose3(X(0), self.priorPose, self.priorNoise))
+            graph.push_back(gtsam.PriorFactorPoint3(X(0), self.priorVel, self.priorVelNoise ))
 
-        ## get factors to use in factor update
-        edges = self.create_factors_from_recent()
+        else: 
+            currentOdomEst = self.get_odom_as_pose3()
+            currentVelEst = vector3(0, 0, 0) ## okay for now, supposed to be bad. Later TODO: replace with DVL estimate?
 
-        node_idx = 0
-        state_idx = 0
+            ## add the current odom as a variable in estimate, to be updated later
+            initial_estimate.insert(X(self.timestep), currentOdomEst)
+            initial_estimate.insert(V(self.timestep), currentVelEst)
+            
+            ## get factors to use in factor update
+            factors = self.create_factors_from_recent() 
 
-        ## TODO we only have one vertex at a time. need some loop to add all previous vertices in estimate
-        for vertex in vertices:
-            graph = gtsam.gtsam.NonlinearFactorGraph()
-            initial_estimate = gtsam.Values()
-            init = False
-            if state_idx == 0:
+            ## add most recent measurements as edges in graph
+            for factor in factors:
+                graph.add(factor)
 
-                ## TODO: graph.push_back or graph.add?
-                graph.add(gtsam.gtsam.PriorFactorPose3(X(0), self.priorPose, self.priorNoise))
-
-                ## TODO add velocity? why Point3?
-                graph.add(gtsam.PriorFactorPoint3(X(0), self.priorVel, self.priorVelNoise ))
-                initial_estimate.insert(X(0), self.priorPose)
-                initial_estimate.insert(V(0), self.priorVel)
-
-                ## TODO update state idx?
-            else: ## TODO this whole else statement is wrong indices
-
-                ## adding all previous poses to initial estimate
-                prevPose = self.optimized_poses.atPose3(vertex.i - 1)
-                initial_estimate.insert(vertex.i, prevPose)
-
-                ## only add current edges (measurements)
-                for edge in edges:
-                    ## TODO for all sensor measurements imu, dvl, etc
-                    odom = gtsam.gtsam.Pose2(edge.x, edge.y, edge.theta)
-                    newFactor = gtsam.gtsam.BetweenFactorPose2(edge.i, edge.j, odom, 
-                        gtsam.noiseModel.Gaussian.Covariance(NOISEFROMEDGE))
-                    graph.add(newFactor)
-            self.isam.update(graph, initial_estimate)                
-            self.optimized_poses = self.isam.calculateEstimate()
+        self.timestep += 1
+        self.isam.update(graph, initial_estimate)                
+        result = self.isam.calculateEstimate()
+        ## TODO do something with resulting estimates
--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import rospy
 import isam
 from sensor_msgs.msg import Imu
 from waterlinked_a50_ros_driver import DVL
+import tf2_ros
 
 def callback_imu(data):
-    auv_slam.update_imu(data.) # whatever this is
+    auv_slam.update_imu(data) # whatever this is
 
 def callback_odom(data):
+    auv_slam.update_odom(data)
     print(data)
 
 def callback_dvl(data):
-    print(data)
-
-def callback_transform(data):
+    auv_slam.update_dvl(data)
     print(data)
 
 ## subscribe to bagfile topics
@@ -20,10 +20,16 @@ def callback_transform(data):
 if __name__=='__main__':
     rospy.init_node('data_listener', anonymous=True)
 
+    tfBuffer = tf2_ros.Buffer()
+    listener = tf2_ros.TransformListener(tfBuffer)
+
     rospy.Subscriber('/mavros/imu/data_raw', Imu, callback_imu)
     #rospy.Subscriber('/mavros/imu/data_raw', sensor_msgs.msg.Imu, callback_odom)
     rospy.Subscriber('/dev/data', DVL, callback_dvl)
 
     auv_slam = isam.AUV_SLAM()
 
-    rospy.spin()
+    while not rospy.is_shutdown():
+        auv_slam.g_transform = tfBuffer.lookup_transform('world', 'base_link', rospy.Time())
+        auv_slam.update_isam()
+        rospy.sleep(100)
