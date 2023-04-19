421f021f8a9d316457bbdfd953dc853633822428
Hersh Vakharia <hershv@umich.edu>
2023-04-19 15:24:37 +0000

    added working code to final branch

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-d = generate_distutils_setup(
-    packages=['uslam'],
-    package_dir={'': 'src'},
-    scripts=['scripts/main.py']
-)
-setup(**d)
